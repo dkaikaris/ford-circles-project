921d07c7684af21c6ca5b05b753836106e6a5c0f
Dominykas <domis.kaikaris@gmail.com>
2025-11-21 21:39:53 +0000

    Initial commit: add python code and generated images

--- ford_circles.py
+++ ford_circles.py
@@ -8,19 +8,19 @@ q_max = 30
 ''' GENERATING FRACTIONS '''
 L = []
 
-for q in range(2, q_max + 1):  # Denominator from 2 to q_max
-    for p in range(1, q):     # Numerator < Denominator
+# Generate unique fractions with 1 <= p <= q <= q_max
+for q in range(2, q_max + 1):  
+    for p in range(1, q): 
         fraction = Rational(p, q)
         L.append((fraction, (fraction.p, fraction.q)))
 
 L.append((Rational(1/1), (1, 1)))
 
-uniqueFractions = {item[0]: item for item in L}  # Remove duplicates
+# Remove duplicate fractions and sort fractions by ascending order
+uniqueFractions = {item[0]: item for item in L} 
 L = list(uniqueFractions.values())
 L.sort(key=lambda x: x[0])
 
-for i in range(len(L)):
-    print(L[i][0], end=', ')
 
 ''' GENERATING FAREY SUMS '''
 fareyCount = 0
@@ -41,7 +41,7 @@ ax.set_title("Ford Circles for Farey Fractions up to q = 30")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 
-# Plot base circle at 0/1
+# Plot base circle at 0/1 since 0/1 not included in list of fractions at first
 circle = plt.Circle((0, 1/2), (1/2), fill=False)
 ax.add_patch(circle)
 
@@ -51,12 +51,9 @@ for frac, (p, q) in L:
     ax.add_patch(circle)
 
 ax.set_aspect('equal', adjustable='box')
-plt.grid(False)
+
 plt.show()
-
-# Save first image
 fig.savefig("farey_ford_circles.png", dpi=300, bbox_inches='tight')
-
 
 
 ''' GOLDEN RATIO (phi) WITH FORD CIRCLES '''
@@ -74,13 +71,13 @@ for i in range(1, 7):
     q = fib[i+1]
     fib_fracs.append((Rational(p, q), (p, q)))
 
-# Plot Ford circles approaching φ−1
+# Plot Ford circles that correspond to Fibonacci fractions
 fig2, ax2 = plt.subplots(figsize=(11, 4))
 
 ax2.axvline(phi_minus_1, color='gold', linestyle='--', linewidth=1)
 ax2.text(phi_minus_1 + 0.0067, 0.11, "φ−1", color='gold', fontsize=12)
 
-# Color map
+# Using colors and labels for ford circles for better visualisation
 cmap = plt.cm.viridis
 colors = [cmap(i / len(fib_fracs)) for i in range(len(fib_fracs))]
 
@@ -91,17 +88,14 @@ for i, ((frac, (p, q))) in enumerate(fib_fracs):
     x = p/q
     r = 1/(2*q**2)
 
-    # Draw circle
     circle = plt.Circle((x, r), r, fill=False, color=colors[i], linewidth=2)
     ax2.add_patch(circle)
 
-    # Legend entry
     handle = plt.Line2D([0], [0], color=colors[i], linewidth=3)
-    label = f"{p}/{q} (radius = {r:.6f})"
+    label = f"{p}/{q} (radius = {r:.5f})"
     legend_handles.append(handle)
     legend_labels.append(label)
 
-# Axis limits
 ax2.set_xlim(phi_minus_1 - 0.17, phi_minus_1 + 0.17)
 ax2.set_ylim(0, 0.12)
 
@@ -113,6 +107,4 @@ ax2.set_aspect('equal', adjustable='box')
 ax2.legend(legend_handles, legend_labels, fontsize=10, loc='upper right')
 
 plt.show()
-
-# Save the φ−1 image
 fig2.savefig("golden_ratio_ford_circles.png", dpi=300, bbox_inches='tight')
